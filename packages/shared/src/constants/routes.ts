94794ee9d1b3566d866b1136d36475f27a9a1953
barrosgoncalo <barrosgoncalobarros@gmail.com>
2026-06-16 13:01:58 +0100

    fix: ignore ios and android folders

--- packages/shared/src/constants/routes.ts
+++
@@ -1,8 +0,0 @@
-export const ROUTES = {
-    SPLASH: 'Splash',
-    LOGIN: 'Login',
-    REGISTER: 'Register',
-    FORGOT_PASSWORD: 'ForgotPassword',
-    WELCOME: 'Welcome',
-    MAIN: 'Main',
-} as const;
