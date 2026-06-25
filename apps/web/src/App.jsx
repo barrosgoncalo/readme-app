9b3cb7b5d6efac1ba7039089c2075c8217b3afbf
barrosgoncalo <barrosgoncalobarros@gmail.com>
2026-06-25 18:35:28 +0100

    remove large video from tracking

--- apps/web/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import AppRouter from './routes/AppRouter.jsx';
-
-export default function App() {
-    return (
-        <div className="app-shell">
-            <AppRouter />
-        </div>
-    );
-}
--- packages/shared/src/utils/colorUtils.js
+++
@@ -1,4 +0,0 @@
-export function withOpacity(hex, opacity) {
-    const alpha = Math.round(opacity * 255).toString(16).padStart(2, '0');
-    return `${hex}${alpha}`;
-}
