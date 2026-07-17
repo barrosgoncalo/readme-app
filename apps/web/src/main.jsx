0458aa4ebdb90facf0b8cd750c40d47c94367fd4
Bernardo <b.lobao@campus.fct.unl.pt>
2026-07-17 03:55:13 +0100

    Integration: Bifurcation between the Admin and User implementations

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from '@readme/shared/src/contexts/AuthContext/web';
+import { ThemeProvider } from './contexts/ThemeContext';
 import App from './App.jsx';
-import './index.css';
+import './styles/global.css';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
-            <AuthProvider>
-                <App />
-            </AuthProvider>
+            <ThemeProvider>
+                <AuthProvider>
+                    <App />
+                </AuthProvider>
+            </ThemeProvider>
         </BrowserRouter>
     </StrictMode>,
 );
